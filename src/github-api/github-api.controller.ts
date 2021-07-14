d06acefd2d8bc4f0ca12a124dfe8647c526d8f3e
Jan-Niclas Strüwer <jan-niclas.struewer@iem.fraunhofer.de>
2021-07-14 11:15:46 +0200

    added sanity check if given github repository exists

--- src/github-api/github-api.controller.ts
+++ src/github-api/github-api.controller.ts
@@ -1,20 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { GithubApiService } from './github-api.service';
 import { RepositoryIdentifierDto } from './model/RepositoryIdentifierDto';
+import { ValidationService } from './validation.service';
 
 @Controller('github-api')
 export class GithubApiController {
-  constructor(private ghApiService: GithubApiService) {}
+  private readonly logger = new Logger(GithubApiController.name);
+  constructor(
+    private ghApiService: GithubApiService,
+    private validationService: ValidationService,
+  ) {}
 
   @Post('diffs')
-  async gatherPullRequestDiffs(
-    @Body() createPullRequestDataDto: RepositoryIdentifierDto,
-  ) {
+  async gatherPullRequestDiffs(@Body() repoIdent: RepositoryIdentifierDto) {
     //TODO:  return a request id and enable to query updates for the running request
     // in order to not have polling we can just introduce a websocket here
-    return this.ghApiService.storePullRequestDiffsForRepo(
-      createPullRequestDataDto,
-    );
+    if (await this.validationService.verify(repoIdent)) {
+      return this.ghApiService.storePullRequestDiffsForRepo(repoIdent);
+    }
   }
 
   @Post('statistics')
@@ -25,9 +28,9 @@ export class GithubApiController {
   }
 
   @Post('tickets')
-  async gatherTickets(
-    @Body() createPullRequestDataDto: RepositoryIdentifierDto,
-  ) {
-    return this.ghApiService.getTickets(createPullRequestDataDto);
+  async gatherTickets(@Body() repoIdent: RepositoryIdentifierDto) {
+    if (await this.validationService.verify(repoIdent)) {
+      return this.ghApiService.getTickets(repoIdent);
+    }
   }
 }
--- src/github-api/github-api.module.ts
+++ src/github-api/github-api.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { GithubApiService } from './github-api.service';
 import { GithubApiController } from './github-api.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { ValidationService } from './validation.service';
 
 @Module({
-  providers: [GithubApiService],
+  providers: [GithubApiService, ValidationService],
   imports: [DatabaseModule],
   controllers: [GithubApiController],
 })
