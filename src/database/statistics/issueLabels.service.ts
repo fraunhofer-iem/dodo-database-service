e7a1ef61c36012a0c28fdc0f123193d4e450d271
zlusch95 <tilschulz@hotmail.de>
2022-01-24 20:11:46 +0100

    db query is ready

--- src/database/statistics/issueLabels.service.ts
+++ src/database/statistics/issueLabels.service.ts
@@ -28,13 +28,6 @@ export class IssueLabels {
       as: 'expandedIssuesWithEvents',
     };
 
-    const getIssueEventTypes = {
-      from: 'issueeventtypes',
-      localField: 'expandedIssuesWithEvents.issueEventTypes',
-      foreignField: '_id',
-      as: 'expandedissueEventTypes',
-    };
-
     const getIssue = {
       from: 'issues',
       localField: 'expandedIssuesWithEvents.issue',
@@ -49,6 +42,20 @@ export class IssueLabels {
       as: 'expandedAssignee',
     };
 
+    const lookupLabels = {
+      from: 'labels',
+      localField: 'expandedIssue.label',
+      foreignField: '_id',
+      as: 'expandedLabels',
+    };
+
+    const lookupAssignees = {
+      from: 'assignees',
+      localField: 'expandedIssue.assignees',
+      foreignField: '_id',
+      as: 'expandedAssignees',
+    };
+
     const query = this.repoModel
       .aggregate()
       .match({ repo: repoId })
@@ -56,13 +63,37 @@ export class IssueLabels {
       .unwind('$issuesWithEvents')
       .lookup(getIssuesWithEvents)
       .unwind('$expandedIssuesWithEvents')
-      //   .lookup(getIssueEventTypes)
-      //   .unwind('$expandedissueEventTypes')
       .lookup(getIssue)
       .unwind('$expandedIssue')
       .project({ expandedIssue: 1 })
       .lookup(getAssignee)
-      .unwind('$expandedAssignee');
+      .unwind('$expandedAssignee')
+      .lookup(lookupAssignees)
+      .unwind('$expandedAssignees')
+      .lookup(lookupLabels)
+      .unwind('expandedLabels')
+      // .project({
+      //   name: '$expandedIssue.name',
+      //   assignee: '$expandedAssignees.login',
+      //   label: '$expandedLabels.name',
+      //   creation: '$expandedIssue.created_at',
+      //   closing: '$expandedIssue.closed_at',
+      // });
+      .group({
+        _id: '$expandedIssue',
+        assignees: { $push: '$expandedAssignees.login' },
+        labels: { $addToSet: '$expandedLabels.name' },
+      });
+    // .unwind('assignees')
+    // .unwind('labels');
+    // .project({
+    //   name: '$_id.title',
+    //   assignee: '$assignees',
+    //   label: '$labels',
+    //   creation: '$_id.created_at',
+    //   closing: { $ifNull: ['$_id.closed_at', Date()] },
+    //   _id: 0,
+    // });
 
     const labels = await query.exec();
     console.log(labels);
