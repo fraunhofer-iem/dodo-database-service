eab261bc9e8174314710a14b1cfe46a74d293d43
zlusch95 <tilschulz@hotmail.de>
2022-03-01 16:23:29 +0100

    deleted single api calls

--- src/kpi/kpi.controller.ts
+++ src/kpi/kpi.controller.ts
@@ -13,53 +13,6 @@ export class KpiController {
     private readonly developerSpreadService: DeveloperSpreadService,
   ) {}
 
-  // @Get('/fcr')
-  // async fcr() {
-  //   return this.issueTrackingService.issueCompletionRate(
-  //     {
-  //       owner: 'octokit',
-  //       repo: 'octokit.js',
-  //     },
-  //     ['bug'],
-  //   );
-  // }
-
-  // @Get('/devSpread')
-  // async devSpread(
-  //   @Query('interval') interval: Intervals = Intervals.MONTH,
-  //   @Query('owner') owner: string,
-  //   @Query('repo') repo?: string,
-  //   @Query('since') since?: string,
-  //   @Query('to') to?: string,
-  // ) {
-  //   this.logger.log('Calculating developer spread for repository:');
-  //   this.logger.log({ owner, repo, interval, since, to });
-  //   return this.developerSpreadService.developerSpread(
-  //     interval,
-  //     owner,
-  //     repo,
-  //     since,
-  //     to,
-  //   );
-  // }
-
-  // @Get()
-  // async getKpis() {
-  //   this.logger.log('Get all KPIs request from user XXX');
-  // }
-
-  // @Get('/releaseCycles')
-  // async getRC(
-  //   @Query('interval') interval: Intervals = Intervals.MONTH,
-  //   @Query('owner') owner: string,
-  //   @Query('repo') repo: string,
-  //   @Query('since') since?: string,
-  //   @Query('to') to?: string,
-  // ) {
-  //   this.logger.log('Get Release Cycle');
-  //   return this.releaseCycle.releaseCycle(interval, owner, repo, since, to);
-  // }
-
   @Get(':id')
   async getKpi(
     @Param('id') id: string,
